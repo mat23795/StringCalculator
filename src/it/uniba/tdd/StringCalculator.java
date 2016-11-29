a243b10664b7cc021333ef7b42e53f6ffe582cea
mat23795 <mat23795@hotmail.it>
2016-11-29 17:15:15 +0100

    Implementate le funzioni principali. Mancano le funzioni e i relativi test che riguardano il delimitatore generico e multiplo

--- src/it/uniba/tdd/StringCalculator.java
+++ src/it/uniba/tdd/StringCalculator.java
@@ -1,9 +1,69 @@
 package it.uniba.tdd;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		// not yet implemented
-		return 0;
+	public int add(String numbersStr) throws StringCalculatorException {
+		int result = -1;
+
+		if (numbersStr == "") {
+			result = 0;
+		} else {
+			if (numbersStr.charAt(0) == '/' && numbersStr.charAt(1) == '/' && numbersStr.contains("[")) {
+				int posFin = numbersStr.indexOf(']');
+				//char delimiter = numbersStr.charAt(2);
+				String strDelim = "";
+				strDelim += numbersStr.substring(3, posFin);
+				String[] strArrayInput = numbersStr.substring(posFin+2).split(strDelim);
+
+				int sum = 0;
+				for (int i = 0; i < strArrayInput.length; i++) {
+					if (Integer.valueOf(strArrayInput[i]) < 0) {
+						throw new StringCalculatorException();
+					}
+					sum += Integer.valueOf(strArrayInput[i]);
+					if (Integer.valueOf(strArrayInput[i]) >= 1000) {
+						sum -= Integer.valueOf(strArrayInput[i]);
+					}
+
+				}
+				result = sum;
+			} else {
+				if (numbersStr.charAt(0) == '/' && numbersStr.charAt(1) == '/') {
+					char delimiter = numbersStr.charAt(2);
+					String strDelim = "";
+					strDelim += delimiter;
+					String[] strArrayInput = numbersStr.substring(4).split(strDelim);
+
+					int sum = 0;
+					for (int i = 0; i < strArrayInput.length; i++) {
+						if (Integer.valueOf(strArrayInput[i]) < 0) {
+							throw new StringCalculatorException();
+						}
+						sum += Integer.valueOf(strArrayInput[i]);
+						if (Integer.valueOf(strArrayInput[i]) >= 1000) {
+							sum -= Integer.valueOf(strArrayInput[i]);
+						}
+
+					}
+					result = sum;
+
+				} else {
+					String[] strArrayInput = numbersStr.split(",|\\n");
+
+					int sum = 0;
+					for (int i = 0; i < strArrayInput.length; i++) {
+						if (Integer.valueOf(strArrayInput[i]) < 0) {
+							throw new StringCalculatorException();
+						}
+						sum += Integer.valueOf(strArrayInput[i]);
+						if (Integer.valueOf(strArrayInput[i]) >= 1000) {
+							sum -= Integer.valueOf(strArrayInput[i]);
+						}
+
+					}
+					result = sum;
+				}
+			}
+		}
+		return result;
 	}
 }
--- src/it/uniba/tdd/tests/StringCalculatorTest.java
+++ src/it/uniba/tdd/tests/StringCalculatorTest.java
@@ -4,11 +4,62 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import it.uniba.tdd.StringCalculator;
+import it.uniba.tdd.StringCalculatorException;
+
 public class StringCalculatorTest {
+	StringCalculator sc = new StringCalculator();
+	String emptyString = "";
+	String arg1String = "1";
+	String stringWith2args = "1,2";
+	String stringWithMoreArg = "1,2,3,4,5,6";
+	String stringWithSlashN = "1\n2,3";
+	String stringWithNegativeNumber = "1,2,-1";
+	String stringWithOneThousand = "1,2,3,1001,4";
+	String stringWithAnotherDelimier = "//;\n1;2";
+	String stringWithAnotherDelimierFormat = "//[***]\n1***2***3";
+	
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void readEmptyStringAndReturnZero() throws StringCalculatorException {
+		assertEquals(sc.add(emptyString),0);
 	}
+	
+	@Test
+	public void readStringWithArg1AndReturn1() throws StringCalculatorException{
+		assertEquals(sc.add(arg1String),1);
+	}
+	
+	@Test
+	public void readA2ArgsString1and2 () throws StringCalculatorException{
+		assertEquals(sc.add(stringWith2args),3);
+	}
+	
+	@Test
+	public void readAMoreArgsStringAndReturn21 () throws StringCalculatorException{
+		assertEquals(sc.add(stringWithMoreArg),21);
+	}
+	
+	@Test
+	public void readStringWithSlashN () throws StringCalculatorException{
+		assertEquals(sc.add(stringWithSlashN),6);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void readStringWithNegativeNumbers () throws StringCalculatorException{
+		int a = sc.add(stringWithNegativeNumber);
+	}
+	
+	@Test 
+	public void readStringWithOneThousandNumbers () throws StringCalculatorException{
+		assertEquals(sc.add(stringWithOneThousand),10);
+	}
+	
+	@Test 
+	public void readStringWithAnotherDelimier  () throws StringCalculatorException{
+		assertEquals(sc.add(stringWithAnotherDelimier),3);
+	}
+	
 
 }
